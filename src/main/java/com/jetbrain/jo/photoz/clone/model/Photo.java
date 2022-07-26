49e654f21f42e2a7d31fa30f9c907d3534770d46
johannpa <johann_paque@hotmail.fr>
2022-07-26 20:08:45 +0200

    spring boot application to download photos

---
+++ src/main/java/com/jetbrain/jo/photoz/clone/model/Photo.java
@@ -0,0 +1,61 @@
+package com.jetbrain.jo.photoz.clone.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Photo {
+
+    private String id;
+
+    @NotEmpty
+    private String fileName;
+
+    private String contentType;
+
+    @JsonIgnore
+    private byte[] data;
+
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    // raw data
+
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
---
+++ src/main/java/com/jetbrain/jo/photoz/clone/service/PhotozService.java
@@ -0,0 +1,41 @@
+package com.jetbrain.jo.photoz.clone.service;
+
+import com.jetbrain.jo.photoz.clone.model.Photo;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+//@Component
+@Service
+public class PhotozService {
+
+
+    private Map<String, Photo> db = new HashMap<>() {{
+        put("1", new Photo("1", "hello.jpg"));
+    }};
+
+    public Collection<Photo> get() {
+        return db.values();
+    }
+
+    public Photo get(String id) {
+        return get(id);
+    }
+
+    public Photo remove(String id) {
+        return db.remove(id);
+    }
+
+    public Photo save(String fileName, String contentType, byte[] data) {
+        Photo photo = new Photo();
+        photo.setContentType(contentType);
+        photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(fileName);
+        photo.setData(data);
+        db.put(photo.getId(), photo);
+        return photo;
+    }
+}
